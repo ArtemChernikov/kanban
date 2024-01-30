397c6206704929e001aff1b46d1c51a923e8c541
Artem Chernikov <overcomingjunk@yandex.ru>
2024-01-31 01:40:10 +0300

    fix: исправить ошибку конструктора в Task

--- src/main/java/ru/praktikum/model/Task.java
+++ src/main/java/ru/praktikum/model/Task.java
@@ -3,7 +3,6 @@ package ru.praktikum.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import ru.praktikum.model.enums.TaskStatus;
 import ru.praktikum.model.enums.TaskType;
 
@@ -15,7 +14,6 @@ import ru.praktikum.model.enums.TaskType;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Task {
     private Long id;
     private String name;
