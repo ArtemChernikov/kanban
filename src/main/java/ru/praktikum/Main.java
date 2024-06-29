8de02166fa7cb97e308565513bd4ee6a5261a96e
Artem Chernikov <overcomingjunk@yandex.ru>
2024-06-29 21:23:58 +0300

    feat: HistoryManager

--- src/main/java/ru/praktikum/Main.java
+++ src/main/java/ru/praktikum/Main.java
@@ -1,6 +1,51 @@
 package ru.praktikum;
 
+import ru.praktikum.model.EpicTask;
+import ru.praktikum.model.SubTask;
+import ru.praktikum.model.Task;
+import ru.praktikum.model.enums.TaskStatus;
+import ru.praktikum.model.enums.TaskType;
+import ru.praktikum.service.HistoryManager;
+import ru.praktikum.service.InMemoryTaskManager;
+import ru.praktikum.service.TaskManager;
+import ru.praktikum.util.Managers;
+
 public class Main {
     public static void main(String[] args) {
+        HistoryManager defaultHistory = Managers.getDefaultHistory();
+        TaskManager inMemoryTaskManager = new InMemoryTaskManager(defaultHistory);
+        EpicTask epicTask = new EpicTask("a", "s", TaskStatus.IN_PROGRESS, TaskType.EPIC_TASK);
+        inMemoryTaskManager.addNewTask(epicTask);
+
+
+        SubTask subTask = new SubTask("s", "s", TaskStatus.DONE, TaskType.SUBTASK, epicTask.getId());
+        inMemoryTaskManager.addNewTask(subTask);
+
+
+        SubTask subTask1 = new SubTask("s", "s", TaskStatus.DONE, TaskType.SUBTASK, epicTask.getId());
+        inMemoryTaskManager.addNewTask(subTask1);
+
+        Task task = new Task("s", "s", TaskStatus.DONE, TaskType.TASK);
+        inMemoryTaskManager.addNewTask(task);
+
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(999L, TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(epicTask.getId(), TaskType.EPIC_TASK));
+        System.out.println(inMemoryTaskManager.getTaskByIdAndType(subTask1.getId(), TaskType.SUBTASK));
+
+        inMemoryTaskManager.getAllTasksByType(TaskType.EPIC_TASK).forEach(System.out::println);
+        System.out.println(inMemoryTaskManager.getHistory().size());
+        inMemoryTaskManager.deleteAllTasksByType(TaskType.SUBTASK);
+        inMemoryTaskManager.getAllTasksByType(TaskType.EPIC_TASK).forEach(System.out::println);
     }
 }
--- src/main/java/ru/praktikum/service/InMemoryHistoryManager.java
+++ src/main/java/ru/praktikum/service/InMemoryHistoryManager.java
@@ -1,9 +1,12 @@
 package ru.praktikum.service;
 
+import ru.praktikum.model.Node;
 import ru.praktikum.model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Artem Chernikov
@@ -11,20 +14,74 @@ import java.util.List;
  * @since 09.02.2024
  */
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_SIZE = 10;
-    private static final int FIRST_HISTORY_ELEMENT = 0;
-    private final List<Task> history = new ArrayList<>();
+    private final CustomLinkedList<Task> history = new CustomLinkedList<>();
+    private final Map<Long, Node<Task>> historyTaskMap = new HashMap<>();
 
     @Override
     public void add(Task task) {
-        if (history.size() == MAX_HISTORY_SIZE) {
-            history.remove(FIRST_HISTORY_ELEMENT);
+        if (historyTaskMap.containsKey(task.getId())) {
+            remove(task.getId());
         }
-        history.add(task);
+        historyTaskMap.put(task.getId(), history.listLast(task));
+    }
+
+    @Override
+    public void remove(long id) {
+        history.removeNode(historyTaskMap.get(id));
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+
+        return history.getTasks();
+    }
+}
+
+class CustomLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    public Node<T> listLast(T element) {
+        Node<T> newNode = new Node<>(null, element, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+        size++;
+        return newNode;
+    }
+
+    public List<T> getTasks() {
+        List<T> allTasks = new ArrayList<>();
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            allTasks.add(currentNode.data);
+            currentNode = currentNode.next;
+        }
+        return allTasks;
+    }
+
+    public void removeNode(Node<T> task) {
+        Node<T> prev = task.prev;
+        Node<T> next = task.next;
+
+        if (task == head && next != null) {
+            next.prev = null;
+            head = next;
+        } else if (task == tail && prev != null) {
+            prev.next = null;
+            tail = prev;
+        } else if (next != null && prev != null) {
+            prev.next = next;
+            next.prev = prev;
+        } else {
+            head = null;
+            tail = null;
+        }
+        size--;
     }
 }
